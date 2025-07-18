9fd4ef9bfbd0c88d8577ad35ab366ba0465d5d35
Knuromuro <138389086+Knuromuro@users.noreply.github.com>
2025-07-19 00:10:16 +0200

    Sort requirements and format plugin

--- bots/plugins/__init__.py
+++ bots/plugins/__init__.py
@@ -2,12 +2,15 @@
 
 from typing import Protocol
 
+
 class IBot(Protocol):
     def start(self) -> None: ...
     def stop(self) -> None: ...
     def status(self) -> str: ...
 
+
 plugins = {}
+
 
 def register(name: str, plugin: IBot) -> None:
     plugins[name] = plugin
